d74b82ab7f5406e55c46cf29b2e5c4177cbeac8e
Hritesh <61223247+hritesh93@users.noreply.github.com>
2020-12-10 19:43:50 +0530

    Refactoring + styling

--- App.vue
+++ App.vue
@@ -1,85 +1,59 @@
 <template>
-    <div class="card">
-        <div class="title">
-            title
-        </div>
-        <div class="content">
-            content
-        </div>
-        <div class="description">
-            description
-        </div>
-    </div>
+  <div class="wrapper">
+    <pokemon-cards 
+      :pokemons="starters" 
+      :selectedId="selectedId"
+      @pokemonClicked="fetchEvolutions"
+    />
+
+    <pokemon-cards :pokemons="evolutions" />
+  </div>
 </template>
 
 <script>
+import Card from './Card.vue'
+import PokemonCards from './PokemonCards.vue'
 const api = 'https://pokeapi.co/api/v2/pokemon'
-const ids = [1, 4, 7]
-
-    export default {
-      data() {
-        return {
-          pokemon: null
-        }
-      },
-
-      // ! Lifecycle Hooks
-      created() {
-        this.fetchData()
-      },
-      // mounted() {
-      //   console.log('mounted')
-      //   console.log(this.$el)
-      // },
-
-      methods: {
-        async fetchData() {
-          // ! for 1 fetch     
-          // const response = await window.fetch(`${api}/1`)
-          // const data = await response.json()
-          // const pokemon = {
-          //   name: data.name,
-          //   sprite: data.sprites.other['official-artwork'].front_default,
-          //   types: data.types.map(type => ({ name: type.type.name }))
-          // }
-          // console.log(pokemon)
-          // ! for multiple fetch
-          const responses = await Promise.all(ids.map(id => window.fetch(`${api}/${id}`)))
-          const data = await Promise.all(responses.map(res => res.json()))
-          const pokemon = data.map(datum => ({
-            name: datum.name,
-            sprite: datum.sprites.other['official-artwork'].front_default,
-            types: datum.types.map(type => ({ name: type.type.name }))
-          }))
-          console.log(pokemon)
-        }
-      }
+const STARTER_IDS = [1, 4, 7]
+export default {
+  components: {
+    Card,
+    PokemonCards
+  },
+  async created() {
+    const starters = await this.fetchData(STARTER_IDS)
+    this.starters = starters
+  },
+  data() {
+    return {
+      starters: [],
+      evolutions: [],
+      selectedId: null
     }
+  },
+  methods: {
+    async fetchEvolutions(pokemon) {
+      this.selectedId = pokemon.id
+      this.evolutions = await this.fetchData([pokemon.id + 1, pokemon.id + 2])
+    },
+    async fetchData(ids) {
+      const responses = await Promise.all(ids.map(id => window.fetch(`${api}/${id}`)))
+      const data = await Promise.all(responses.map(res => res.json()))
+      return data.map(datum => ({
+        id: datum.id,
+        name: datum.name,
+        sprite: datum.sprites.other['official-artwork'].front_default,
+        types: datum.types.map(type => type.type.name)
+      }))
+    }
+  }
+}
 </script>
 
 <style scoped>
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-.title, .content, .description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-.title, .content {
-  border-bottom: 1px solid silver;
-}
-.title {
-  font-size: 1.25em;
-}
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgrey;
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
